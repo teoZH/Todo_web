99c4b221f4c6ec0801f46a6570eb4a5aa4e3442b
teoZH <58920759+teoZH@users.noreply.github.com>
2020-10-16 14:13:01 +0300

    added basic_bot_catcher

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 
+def clean_bot_catcher(bot_catcher):
+    if len(bot_catcher) > 0:
+        raise forms.ValidationError('Gotcha BOT')
+    return bot_catcher
+
+
 class TodoInput(forms.Form):
-    title = forms.CharField(label='Title', max_length=20,min_length=6)
+    title = forms.CharField(label='Title', max_length=20, min_length=6)
     description = forms.CharField(widget=forms.Textarea())
+    bot_catcher = forms.CharField(widget=forms.HiddenInput(), required=False, validators=[clean_bot_catcher])
